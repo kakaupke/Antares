8d56283a51bada87915d4fa549daf308c4791192
kakaupke <37269109+kakaupke@users.noreply.github.com>
2018-03-11 20:51:07 -0400

    Initial Commit

---
+++ AstronomyTweets.js
@@ -0,0 +1,61 @@
+/*
+Written by: Katie Kaupke
+Date: 2018-03-11
+
+Description: 
+1. Select a list of the top 100 tweets with a hashtag of your choice (Node.js)
+2. Output results to the console or use a GET rest endpoint (express.js + Node.js)
+
+Node.js Version: v8.10.0
+npm Version: v5.6.0
+
+Requires npm install Twitter
+*/
+
+
+console.log("This is Major Tom to Ground Control: ");
+//required 
+var rocket = require('Twitter'); //Twitter
+var crew = require('./config'); //Config file
+
+//The Twitter object
+var mission = new rocket(crew); 
+
+//Search parameters for the 100 most recent #Astronomy tweets
+var trajectory = {
+q: '#Astronomy',
+result_type: 'recent',
+count: 100
+} 
+
+//Get the data
+mission.get('search/tweets', trajectory, blastOff); 
+
+//return the formatted data
+var itinerary;
+
+function blastOff(error, tweets, response) {
+
+	for (i = 0; i < 100; i++) {
+		
+		try{
+			itinerary = '\ntweet #' + (i + 1) + ', From: @' + tweets.statuses[i]["user"]["screen_name"] 
+							+ '\n' + tweets.statuses[i]["text"] + '\n' ;
+
+		console.log(itinerary); //print out results 
+
+		}catch(e){ //basic error handling
+
+			console.log("This is Ground Control to Major Tom: Result " + i + 'is not a valid tweet');
+		}
+
+	}
+
+} 
+
+
+
+
+
+
+
---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+ consumer_key:         'c12kcHdK8BMcTWls9TXVzIwWV',
+  consumer_secret:      'FifuPr9jgVdF5kzodptpJzlixUTdTJy73D79nHiulrbg5XjmHh',
+  access_token_key:         '1968939859-QhWs9QObOlU37qoBDcMzVJPQMynEJWCWeCSQtNq',
+  access_token_secret:  '7PiUs55MvX5gDw5SerFIDvlfWDtuDUvWamMwFeBPnlTfQ'
+}
